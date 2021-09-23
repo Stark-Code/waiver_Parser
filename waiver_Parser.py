0eadfd661b1546d83354fc1621f14a5f115ca5a1
John <starkweather_john@yahoo.com>
2021-09-23 13:41:32 -0400

    Added Prefilter Fixed minor bug for * fields

--- waiver_Parser.py
+++ waiver_Parser.py
@@ -2,21 +2,33 @@ from pdfminer.high_level import extract_text  # Install via  pip install pdfmine
 import re
 
 
+def process_Text(text, _filter):
+    if re.search(_filter, text) is None:
+        return text
+    text = re.sub(_filter, '', text)
+    return process_Text(text, _filter)
+
+
 def parse_Waiver(waiver):
-    text = extract_text(waiver)
+    pre_Filters = [r"IP: \d{2}.*", r"Page .*"]
+    waiver_Text = extract_text(waiver)
+
+    for filter in pre_Filters:
+        waiver_Text = process_Text(waiver_Text, filter)
+    print(waiver_Text)
     # print(text)
-    origin = re.search('(?<=Completed:) ?\d{4}-\d{2}-\d{2}', text)
-    first_Name = re.search('.*\n?(?=First Name*)', text)
-    middle_Name = re.search('.*\n?(?=Middle Name*)', text)
-    last_Name = re.search('.*\n?(?=Last Name*)', text)
-    phone = re.search('.*\n?(?=Phone*)', text)
-    address_1 = re.search('(?<=Address Line 1:\*\n)\n?(.*)', text)
-    address_2 = re.search('(?<=Address Line 2:\n)\n?(.*)', text)
-    country = re.search('(?<=Country:\*\n)\n?(.*)', text)
-    city = re.search('(?<=City:\*\n)\n?(.*)', text)
-    state = re.search('(?<=State/Province:\*\n)\n?(.*)', text)
-    zip_Code = re.search('(?<=Zip/Postal:\*\n)\n?(.*)', text)
-    email = re.search('(?<=Email Address\n)\n?(.*)', text)
+    origin = re.search('(?<=Completed:) ?\d{4}-\d{2}-\d{2}', waiver_Text)
+    first_Name = re.search('.*\n?(?=First Name*)', waiver_Text)
+    middle_Name = re.search('.*\n?(?=Middle Name*)', waiver_Text)
+    last_Name = re.search('.*\n?(?=Last Name*)', waiver_Text)
+    phone = re.search('.*\n?(?=Phone*)', waiver_Text)
+    address_1 = re.search('(?<=Address Line 1:\*\n)\n?(.*)', waiver_Text)
+    address_2 = re.search('(?<=Address Line 2:\n)\n?(.*)', waiver_Text)
+    country = re.search('(?<=Country:\*\n)\n?(.*)', waiver_Text)
+    city = re.search('(?<=City:\*\n)\n?(.*)', waiver_Text)
+    state = re.search('(?<=State/Province:\*\n)\n?(.*)', waiver_Text)
+    zip_Code = re.search('(?<=Zip/Postal:\*\n)\n?(.*)', waiver_Text)
+    email = re.search('(?<=Email Address\n)\n?(.*)', waiver_Text)
     print(f'Date of origin: {origin.group(0)}')
     print(f'First Name: {first_Name.group(0).strip()}')
     print(f'Middle Name: {middle_Name.group(0).strip()}')
@@ -30,7 +42,8 @@ def parse_Waiver(waiver):
     print(f'State: {state.group(1).strip()}')
     print(f'Zip: {zip_Code.group(1).strip()}')
 
+
 waiver_File = open("/home/john/Downloads/Starkweather-John-20210922-33pw72b3LgYYABmDFJNY7Y.pdf", "rb")
-# waiver_File = open("Paste Path Here", "rb")
+
 
 parse_Waiver(waiver_File)
